0305fae35e2234ffe286cb949ed4e59f1d90bbfe
Balint Nagy <bnagy@cloudera.com>
2024-12-04 13:46:49 +0100

    Add solution for day 4, task 2

--- tasks/day4.go
+++ tasks/day4.go
@@ -85,6 +85,63 @@ func yStaysStill(y int) int {
 	return y
 }
 
+type Day4Task2 struct{}
+
+func (Day4Task2) CalculateAnswer(input string) (string, error) {
+	lines := utils.Filter(strings.Split(input, "\n"), func(s string) bool {
+		return s != ""
+	})
+
+	var letters Letters
+	for _, l := range lines {
+		letters = append(letters, []rune(l))
+	}
+	crossmasCount := 0
+	for y := 1; y < len(letters)-1; y++ {
+		for x := 1; x < len(letters[y])-1; x++ {
+			p := Pos{x, y}
+			if letters.hasCrossMasAt(p) {
+				crossmasCount++
+			}
+		}
+	}
+	return strconv.Itoa(crossmasCount), nil
+}
+
+// pos will be used to locate the 'A' of the cross mas
+func (l Letters) hasCrossMasAt(pos Pos) bool {
+	if pos.x < 1 || pos.y < 1 || pos.y >= len(l)-1 || pos.x >= len(l[pos.y])-1 {
+		return false
+	}
+	if !l.isLetterAt(pos, 'A') {
+		return false
+	}
+	topLeftLetter := l.letterAt(Pos{pos.x - 1, pos.y - 1})
+	topRightLetter := l.letterAt(Pos{pos.x - 1, pos.y + 1})
+	bottomLeftLetter := l.letterAt(Pos{pos.x + 1, pos.y - 1})
+	bottomRightLetter := l.letterAt(Pos{pos.x + 1, pos.y + 1})
+	lettersCounts := make(map[rune]int)
+	lettersCounts[topLeftLetter]++
+	lettersCounts[topRightLetter]++
+	lettersCounts[bottomLeftLetter]++
+	lettersCounts[bottomRightLetter]++
+	if lettersCounts['M'] != 2 {
+		return false
+	}
+	if lettersCounts['S'] != 2 {
+		return false
+	}
+	if topLeftLetter == bottomRightLetter {
+		return false
+	}
+	log.Println("X-Mas found at ", pos)
+	return true
+}
+
+func (l Letters) letterAt(pos Pos) rune {
+	return l[pos.y][pos.x]
+}
+
 func (l Letters) hasXmasFrom(pos Pos, xStep, yStep func(int) int) bool {
 	if !l.isLetterAt(pos, 'X') {
 		return false
