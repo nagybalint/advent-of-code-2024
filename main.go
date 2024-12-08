d0bd0ef206abcf909daf719e2d0f28b7d97ce776
Balint Nagy <bnagy@cloudera.com>
2024-12-08 13:34:01 +0100

    Add solution for day 8, task 2

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/nagybalint/advent-of-code-2024/tasks"
@@ -9,13 +10,13 @@ import (
 )
 
 func main() {
-	//log.SetOutput(io.Discard)
+	log.SetOutput(io.Discard)
 	input, err := utils.ReadFileFromRelative("resources/day8.txt")
 	if err != nil {
 		log.Println("Error reading input")
 		panic(err)
 	}
-	d := tasks.Day8Task1{}
+	d := tasks.Day8Task2{}
 	answer, err := d.CalculateAnswer(input)
 	if err != nil {
 		log.Fatalln("Cannot calculate answer", err)
--- tasks/day8.go
+++ tasks/day8.go
@@ -49,6 +49,44 @@ func (Day8Task1) CalculateAnswer(input string) (string, error) {
 	return strconv.Itoa(len(antinodes)), nil
 }
 
+type Day8Task2 struct{}
+
+func (Day8Task2) CalculateAnswer(input string) (string, error) {
+	var city cityLayout
+	for _, line := range utils.Filter(strings.Split(input, "\n"), utils.IsNonEmptyString) {
+		var cityLine []rune
+		for _, r := range line {
+			cityLine = append(cityLine, r)
+		}
+		city = append(city, cityLine)
+	}
+	antennas := city.findAntennas()
+	grouped := utils.GroupBy(antennas, func(a antenna) rune { return a.typ })
+	antinodes := make(sets.Set[utils.Pos])
+	for _, antennas := range grouped {
+		for pair := range utils.Pairs(antennas) {
+			antinodes.Insert(getAllReflections(city, pair[0].p, pair[1].p)...)
+			antinodes.Insert(getAllReflections(city, pair[1].p, pair[0].p)...)
+		}
+	}
+	return strconv.Itoa(len(antinodes)), nil
+}
+
+func getAllReflections(city cityLayout, p utils.Pos, center utils.Pos) (reflections []utils.Pos) {
+	reflections = append(reflections, center)
+	for {
+		reflection := p.Reflect(center)
+		if city.isInBounds(reflection) {
+			reflections = append(reflections, reflection)
+			p = center
+			center = reflection
+		} else {
+			break
+		}
+	}
+	return reflections
+}
+
 func (layout cityLayout) isInBounds(p utils.Pos) bool {
 	if p.X < 0 || p.Y < 0 {
 		return false
